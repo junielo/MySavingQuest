4d345c3584653be848b578f4f41efed9887b8eff
Junielo <jun.guardalupe@gmail.com>
2025-09-01 23:28:42 +0800

    Added gitignore

--- app/src/main/java/com/calikot/mysavingquest/db/client/connection.kt
+++ app/src/main/java/com/calikot/mysavingquest/db/client/connection.kt
@@ -5,7 +5,7 @@ import io.github.jan.supabase.postgrest.Postgrest
 
 object connection {
     val supabase = createSupabaseClient(
-        supabaseUrl = "https://xyzcompany.supabase.co",
+        supabaseUrl = "https://fasdfa.supabase.co",
         supabaseKey = "your_public_anon_key"
     ) {
         install(Postgrest)
